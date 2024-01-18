c843fe77b193116d4fe5f88ead7859a215b7a1c7
Feyyaz Numan Cavlak <feyyazcavlak@outlook.com>
2024-01-19 02:11:44 +0300

    Implenemnt Web3Modal and Wagmi

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,10 +1,49 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import "@walletconnect/react-native-compat";
+import { WagmiConfig } from "wagmi";
+import { mainnet, polygon, arbitrum } from "viem/chains";
+import {
+  createWeb3Modal,
+  defaultWagmiConfig,
+  Web3Modal,
+} from "@web3modal/wagmi-react-native";
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = "262c771eea4450d6fddf9d3cea40a369";
+
+// 2. Create config
+const metadata = {
+  name: "GHOPPA",
+  description:
+    "Ghoppa is a wallet for GHO tokens, and it allows you to swap GHO for other tokens.",
+  url: "https://itublockchain.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+  redirect: {
+    native: "ghoppa://",
+    universal: "itublockchain.com",
+  },
+};
+
+const chains = [mainnet, polygon, arbitrum];
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createWeb3Modal({
+  projectId,
+  chains,
+  wagmiConfig,
+});
+
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeAreaProvider>
+    <WagmiConfig config={wagmiConfig}>
+      <SafeAreaProvider>
+        <Stack screenOptions={{ headerShown: false }} />
+        <Web3Modal />
+      </SafeAreaProvider>
+    </WagmiConfig>
   );
 }
--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,12 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, Button, Pressable } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useWeb3Modal } from "@web3modal/wagmi-react-native";
+import { useAccount } from "wagmi";
 
 const Home = () => {
   const insets = useSafeAreaInsets();
+  const { open } = useWeb3Modal();
+  const { address } = useAccount();
 
   return (
     <View style={{ paddingTop: insets.top }}>
-      <Text>Home</Text>
+      <Pressable onPress={() => open()}>
+        <Text>{address ? address : "Connect Wallet"}</Text>
+      </Pressable>
     </View>
   );
 };
